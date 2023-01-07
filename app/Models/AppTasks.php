2c031e90715f4f41fe2e7e814184da7b2d1823e5
MrJung988 <karkisamir99@gmail.com>
2023-01-07 12:53:11 +0545

    Task 14/  AppTasks migration, Model added

---
+++ app/Models/AppTasks.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppTasks extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'task_parent_id',
+        'taskName',
+        'Description',
+        'main_task_parent_id',
+        'by_user_id',
+        'to_user_id',
+        'dt_starting',
+        'dt_done',
+        'is_done',
+        'is_active',
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(AppTasks::class, 'task_parent_id');
+    }
+
+    public function mainParent()
+    {
+        return $this->belongsTo(AppTasks::class, 'main_task_parent_id');
+    }
+
+    public function byUser()
+    {
+        return $this->belongsTo(User::class, 'by_user_id');
+    }
+
+    public function toUser()
+    {
+        return $this->belongsTo(User::class, 'to_user_id');
+    }
+
+}
---
+++ database/migrations/2023_01_07_053204_create_app_tasks_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('task_parent_id')->nullable();
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedBigInteger('main_task_parent_id')->nullable();
+            $table->unsignedBigInteger('by_user_id');
+            $table->unsignedBigInteger('to_user_id');
+            $table->timestamp('dt_starting')->nullable();
+            $table->timestamp('dt_done')->nullable();
+            $table->boolean('is_done')->default(0);
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+
+            $table->foreign('task_parent_id')->references('id')->on('app_tasks')->onDelete('set null');
+            $table->foreign('main_task_parent_id')->references('id')->on('app_tasks')->onDelete('set null');
+            $table->foreign('by_user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('to_user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('app_tasks', function (Blueprint $table) {
+            $table->dropForeign(['task_parent_id']);
+            $table->dropForeign(['main_task_parent_id']);
+            $table->dropForeign(['by_user_id']);
+            $table->dropForeign(['to_user_id']);
+        });
+
+        Schema::dropIfExists('app_tasks');
+    }
+};
